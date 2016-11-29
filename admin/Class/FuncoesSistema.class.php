fa4206fe7c68207093a8842396e767dd42a2a0f5
LeoBessa403 <leonardomcbessa@gmail.com>
2016-11-29 17:39:11 -0200

    Alteração da Estrutura do padrão do projeto

--- admin/Class/FuncoesSistema.class.php
+++ admin/Class/FuncoesSistema.class.php
@@ -3,21 +3,24 @@
 /**
  * Check.class [ HELPER ]
  * Classe responável por manipular e validade dados do sistema!
- * 
+ *
  * @copyright (c) 2014, Leo Bessa
  */
-class FuncoesSistema {
+class FuncoesSistema
+{
 
-    public static function retornoCheckbox($check){
-        if($check):
+    public static function retornoCheckbox($check)
+    {
+        if ($check):
             return "S";
         else:
             return "N";
         endif;
     }
-    
-    public static function OperacaoAuditoria($operacao){
-        
+
+    public static function OperacaoAuditoria($operacao)
+    {
+
         switch ($operacao) {
             case "I":
                 $op = '<span class="label label-success">INSERIU</span>';
@@ -29,13 +32,14 @@ class FuncoesSistema {
                 $op = '<span class="label label-warning">ATUALIZOU</span>';
                 break;
         }
-        
+
         return $op;
     }
-    
-    
-    public static function SituacaoUsuario($st){
-        
+
+
+    public static function SituacaoUsuario($st)
+    {
+
         switch ($st) {
             case "A":
                 $op = '<span class="label label-success">ATIVO</span>';
@@ -44,12 +48,13 @@ class FuncoesSistema {
                 $op = '<span class="label label-danger">INATIVO</span>';
                 break;
         }
-        
+
         return $op;
     }
-    
-    public static function SituacaoSimNao($st){
-        if(!$st):
+
+    public static function SituacaoSimNao($st)
+    {
+        if (!$st):
             $st = "N";
         endif;
         switch ($st) {
@@ -60,31 +65,33 @@ class FuncoesSistema {
                 $op = '<span class="label label-danger">NÃO</span>';
                 break;
         }
-        
+
         return $op;
     }
-    
-    public static function GetDadosContato($dados){
-        $contato['nu_tel1']         = (!empty($dados['nu_tel1'])? $dados['nu_tel1'] : '');
-        $contato['nu_tel2']         = (!empty($dados['nu_tel2'])? $dados['nu_tel2'] : '');
-        $contato['nu_tel3']         = (!empty($dados['nu_tel3'])? $dados['nu_tel3'] : '');
-        $contato['nu_tel_0800']     = (!empty($dados['nu_tel_0800'])? $dados['nu_tel_0800'] : '');
-        $contato['ds_site']         = (!empty($dados['ds_site'])? $dados['ds_site'] : '');
-        $contato['ds_email']        = (!empty($dados['nu_tel2'])? $dados['ds_email'] : '');
-        
+
+    public static function GetDadosContato($dados)
+    {
+        $contato['nu_tel1'] = (!empty($dados['nu_tel1']) ? $dados['nu_tel1'] : '');
+        $contato['nu_tel2'] = (!empty($dados['nu_tel2']) ? $dados['nu_tel2'] : '');
+        $contato['nu_tel3'] = (!empty($dados['nu_tel3']) ? $dados['nu_tel3'] : '');
+        $contato['nu_tel_0800'] = (!empty($dados['nu_tel_0800']) ? $dados['nu_tel_0800'] : '');
+        $contato['ds_site'] = (!empty($dados['ds_site']) ? $dados['ds_site'] : '');
+        $contato['ds_email'] = (!empty($dados['nu_tel2']) ? $dados['ds_email'] : '');
+
         return $contato;
     }
-    
-    public static function GetDadosEndereco($dados){
-        $endereco['ds_endereco']     = (!empty($dados['ds_endereco'])? $dados['ds_endereco'] : '');
-        $endereco['ds_complemento']  = (!empty($dados['ds_complemento'])? $dados['ds_complemento'] : '');
-        $endereco['ds_bairro']       = (!empty($dados['ds_bairro'])? $dados['ds_bairro'] : '');
-        $endereco['no_cidade']       = (!empty($dados['no_cidade'])? $dados['no_cidade'] : '');
-        $endereco['nu_cep']          = (!empty($dados['nu_cep'])? $dados['nu_cep'] : '');
-        $endereco['sg_uf']           = (!empty($dados['sg_uf'])? $dados['sg_uf'][0] : '');
-        
+
+    public static function GetDadosEndereco($dados)
+    {
+        $endereco['ds_endereco'] = (!empty($dados['ds_endereco']) ? $dados['ds_endereco'] : '');
+        $endereco['ds_complemento'] = (!empty($dados['ds_complemento']) ? $dados['ds_complemento'] : '');
+        $endereco['ds_bairro'] = (!empty($dados['ds_bairro']) ? $dados['ds_bairro'] : '');
+        $endereco['no_cidade'] = (!empty($dados['no_cidade']) ? $dados['no_cidade'] : '');
+        $endereco['nu_cep'] = (!empty($dados['nu_cep']) ? $dados['nu_cep'] : '');
+        $endereco['sg_uf'] = (!empty($dados['sg_uf']) ? $dados['sg_uf'][0] : '');
+
         return $endereco;
     }
-    
-        
- }
+
+
+}
